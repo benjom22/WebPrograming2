9e025b1e4d8e570da0cf08613c7d4266f4a880a9
benjom22 <benjom@bih.net.ba>
2023-04-21 15:18:54 +0200

    Midterm prep

--- index.php
+++ index.php
@@ -9,8 +9,35 @@ array('mysql:host=localhost;dbname=lab3_db','root',''));
 
 Flight::route('GET /api/users', function(){
     $users = Flight::db()->query('SELECT * FROM Users', PDO::FETCH_ASSOC)->fetchAll();
-    var_dump($users);
+    //var_dump($users);
     Flight::json($users);
+});
+
+Flight::route('GET /api/usersid', function(){
+    $users = Flight::db()->query('SELECT id, firstName FROM Users', PDO::FETCH_ASSOC)->fetchAll();
+    //var_dump($users);
+    Flight::json($users);
+});
+
+Flight::route('GET /api/usersid/@id', function($id){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $schema = "lab3_db";
+    $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+      /** TODO
+      * write a query that will list investor by given id
+      *
+      * This endpoint should return output in JSON format
+      */
+  
+      $stmt = $conn->prepare("SELECT * FROM Users where id = $id");
+      $stmt->execute();
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      Flight::json(reset($result));
 });
 
 Flight::start();
